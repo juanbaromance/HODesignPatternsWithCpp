02643a035eebc999530ff68d0d7c56de72fadc7e
juanba <juanba.romance@gmail.com>
2019-11-23 13:33:46 +0100

    Type Erasure as a technic for generic algorithms, performance impacts measurements on the smart pointers case of use.

--- Chapter06/smartptr.C
+++ Chapter06/smartptr.C
@@ -7,12 +7,12 @@
 
 template <typename T, typename Deleter>
 class smartptr {
-    public:
+public:
     smartptr(T* p, Deleter d) : p_(p), d_(d) {}
     ~smartptr() { d_(p_); }
     T* operator->() { return p_; }
     const T* operator->() const { return p_; }
-    private:
+private:
     T* p_;
     Deleter d_;
 };
@@ -23,17 +23,19 @@ class smartptr_te {
         virtual void apply(void*) = 0;
         virtual ~deleter_base() {}
     };
-    template <typename Deleter> struct deleter : public deleter_base {
+    template <typename Deleter>
+    struct deleter : public deleter_base {
         deleter(Deleter d) : d_(d) {}
         virtual void apply(void* p) { d_(static_cast<T*>(p)); }
         Deleter d_;
     };
-    public:
-    template <typename Deleter> smartptr_te(T* p, Deleter d) : p_(p), d_(new deleter<Deleter>(d)) {}
+public:
+    template <typename Deleter>
+    smartptr_te(T* p, Deleter d) : p_(p), d_(new deleter<Deleter>(d)) {}
     ~smartptr_te() { d_->apply(p_); delete d_; }
     T* operator->() { return p_; }
     const T* operator->() const { return p_; }
-    private:
+private:
     T* p_;
     deleter_base* d_;
 };
@@ -44,15 +46,17 @@ class smartptr_te_lb {
         virtual void apply(void*) = 0;
         virtual ~deleter_base() {}
     };
-    template <typename Deleter> struct deleter : public deleter_base {
+    template <typename Deleter>
+    struct deleter : public deleter_base {
         deleter(Deleter d) : d_(d) {}
         virtual void apply(void* p) { d_(static_cast<T*>(p)); }
         Deleter d_;
     };
-    public:
-    template <typename Deleter> smartptr_te_lb(T* p, Deleter d) :
-        p_(p),
-        d_(sizeof(Deleter) > sizeof(buf_) ? new deleter<Deleter>(d) : new (buf_) deleter<Deleter>(d)) 
+public:
+    template <typename Deleter>
+    smartptr_te_lb(T* p, Deleter d) :
+          p_(p),
+          d_(sizeof(Deleter) > sizeof(buf_) ? new deleter<Deleter>(d) : new (buf_) deleter<Deleter>(d))
     {}
     ~smartptr_te_lb() {
         d_->apply(p_);
@@ -64,7 +68,7 @@ class smartptr_te_lb {
     }
     T* operator->() { return p_; }
     const T* operator->() const { return p_; }
-    private:
+private:
     T* p_;
     deleter_base* d_;
     char buf_[16];
